c0a675934774d6bb6d17d9cd55b66aa5d666c734
Duong Pham <duongnapham@qasymphony.com>
2018-06-07 17:47:20 +0700

    import source code of dot net sample

---
+++ NUnitReportSample_Source/NUnitReportSample.Test/BrowserTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using NUnitReportSample.Library;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace NUnitReportSample.Test
+{
+    [TestFixture]
+    public class BrowserTest
+    {
+        [Test]
+        public void ShouldGoToGoogleSite()
+        {
+            const string PROCESS_NAME = "IExplore.exe";
+            const int SLEEP = 3000;
+
+            Process process = Process.Start(PROCESS_NAME);
+            Process[] pname = Process.GetProcessesByName("IExplore");
+            Assert.That(pname.Length != 0);
+            Thread.Sleep(SLEEP);
+            process.Kill();
+        }
+    }
+}
---
+++ NUnitReportSample_Source/NUnitReportSample.Test/CalculatorTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using NUnitReportSample.Library;
+
+namespace NUnitReportSample.Test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            Calculator cal = new Calculator();
+            int expectedResult = cal.Add(7, 8);
+            Assert.That(expectedResult, Is.EqualTo(15));
+        }
+
+        [Test]
+        public void ShouldMultipleTwoNumbers()
+        {
+            Calculator cal = new Calculator();
+            int expectedResult = cal.Mul(5, 10);
+            Assert.That(expectedResult, Is.EqualTo(20));
+        }
+    }
+}
